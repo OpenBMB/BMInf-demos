60a7a42c382840c8172cd16741ddf70734eefe71
jctime <jctime@163.com>
2021-09-24 12:24:11 +0800

    merger lock

--- main.py
+++ main.py
@@ -15,57 +15,44 @@ app = Flask(__name__, static_url_path="", static_folder="statics")
 model = None
 #lock methods
 lock = threading.Lock()
-#lock model
-lock_model = threading.Lock()
 
 @app.route('/api/loadmodel',methods=['GET'])
 def model_load():
     res = request.args.get('key')
     key = int(res)
     global model
-    if key == 1 and lock.locked() == False and lock_model.locked() == False:
-        lock_model.acquire()
+    if key == 1 and lock.locked() == False:
+        lock.acquire()
         model = None
         print('loading cpm1')
         model = bminf.models.CPM1()
-        data={}
-        data['code']=200
-        data['message']='success'
-        lock_model.release()
+        data={'code':200,'message':'success'}
+        lock.release()
         return jsonify(data)
-    elif key == 2 and lock.locked() == False and lock_model.locked() == False:
-        lock_model.acquire()
+    elif key == 2 and lock.locked() == False:
+        lock.acquire()
         model = None
         print('loading cpm2')
         model = bminf.models.CPM2()
-        data={}
-        data['code']=200
-        data['message']='success'
-        lock_model.release()
+        data={'code':200,'message':'success'}
+        lock.release()
         return jsonify(data)
-    elif key == 3 and lock.locked() == False and lock_model.locked() == False:
-        lock_model.acquire()
+    elif key == 3 and lock.locked() == False:
+        lock.acquire()
         model = None
         print('loading eva')
         model = bminf.models.EVA()
-        data={}
-        data['code']=200
-        data['message']='success'
-        lock_model.release()
+        data={'code':200,'message':'success'}
+        lock.release()
         return jsonify(data)
     else:
-        data={}
-        data['code']=201
-        data['message']='sorry，此模型正在被其他人独霸，请稍后再试！'
+        data={'code':201,'message':'sorry，此模型正在被其他人独霸，请稍后再试！'}
         return jsonify(data),201 
 
 @app.route('/api/gpuinfo',methods=['GET'])
 def gpu_info():
     rate = gpuinfo.gpu_info()
-    data={}
-    data['code']=200
-    data['message']='success'
-    data['data']=rate
+    data={'code':200,'message':'success','data':rate}
     return jsonify(data)
 
 @app.route('/api/fillblank',methods=['POST'])
@@ -73,17 +60,11 @@ def fillBlank():
     if lock.locked() == False:
         lock.acquire()
         result = fillblank.fillBlank(model)
-        data={}
-        data['code']=200
-        data['message']='success'
-        data['data']=result
+        data={'code':200,'message':'success','data':result}
         lock.release()
         return jsonify(data)
     else:
-        print('using')
-        data={}
-        data['code']=201
-        data['message']='sorry，此接口正在被其他人独霸，请稍后再试！'
+        data={'code':201,'message':'sorry，此接口正在被其他人独霸，请稍后再试！'}
         return jsonify(data),201
 
 @app.route('/api/generatestory',methods=['POST'])
@@ -91,17 +72,11 @@ def generateStory():
     if lock.locked() == False:
         lock.acquire()
         result = story.generateStory(model)
-        data={}
-        data['code']=200
-        data['message']='success'
-        data['data']=result
+        data={'code':200,'message':'success','data':result}
         lock.release()
         return jsonify(data)
     else:
-        print('using')
-        data={}
-        data['code']=201
-        data['message']='sorry，此接口正在被其他人独霸，请稍后再试！'
+        data={'code':201,'message':'sorry，此接口正在被其他人独霸，请稍后再试！'}
         return jsonify(data),201
 
 @app.route('/api/dialogue',methods=['POST'])
@@ -109,17 +84,11 @@ def generateDialogue():
     if lock.locked() == False:
         lock.acquire()
         result = dialogue.dialogue(model)
-        data={}
-        data['code']=200
-        data['message']='success'
-        data['data']=result
+        data={'code':200,'message':'success','data':result}
         lock.release()
         return jsonify(data)
     else:
-        print('using')
-        data={}
-        data['code']=201
-        data['message']='sorry，此接口正在被其他人独霸，请稍后再试！'
+        data={'code':201,'message':'sorry，此接口正在被其他人独霸，请稍后再试！'}
         return jsonify(data),201
 
 @app.route("/", methods=["GET"])
